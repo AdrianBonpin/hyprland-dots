c61567fdd1b8677356286156347e58b787541f68
Adrian Alfred C. Bonpin <adrianbonpin@gmail.com>
2024-03-09 17:31:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-echo "Enter Password Below"
-sudo pacman -S fish git alacritty dunst rofi waybar thunderbird unzip nemo tlp tldr nwg-look ttf-noto-nerd noto-fonts ttf-font-awesome --noconfirm
-echo "Set to Fish"
-echo /usr/local/bin/fish | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
+echo "Installing Fish"
+sudo pacman -S fish
+echo /usr/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/bin/fish
+
 echo "Copy to .config"
-cp -a / ~/.config/
+cp -r /alacritty ~/.config/
+cp -r /dunst ~/.config/
+cp -r /fish ~/.config/
+cp -r /hypr ~/.config/
+cp -r /rofi ~/.config/
+cp -r /wlogout ~/.config/
+
 echo "Installing yay"
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
-echo "Installing System Sht"
-yay -S wl-paste cliphist hyprpaper iwgtk wlogout blueman pavucontrol hyprpicker grim slurp wpctl brillo brightnessctl microsoft-edge-stable-bin visual-studio-code-bin eza --noconfirm
+
+echo "Install Catppuccin Mocha GTK Theme"
+yay -S catppuccin-gtk-theme-mocha
+
+echo "Installing"
+sudo pacman -S ttf-noto-nerd noto-fonts ttf-font-awesome --noconfirm
+yay -S dunst rofi thunderbird unzip neo tlp nwg-look nwg-displays wl-paste eww cliphist hyprpaper iwgtk wlogout blueman pavucontrol hyprpicker grim slurp wpctl brillo brightnessctl microsoft-edge-stable-bin visual-studio-code-bin eza --noconfirm
