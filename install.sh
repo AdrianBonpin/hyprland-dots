8d818a1b1e4909a2e5b4ed485c9c7b142a7739bd
Adrian Alfred C. Bonpin <adrianbonpin@gmail.com>
2024-03-09 17:41:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,12 @@ sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/ya
 echo "Install Catppuccin Mocha GTK Theme"
 yay -S catppuccin-gtk-theme-mocha
 
-echo "Installing"
-sudo pacman -S ttf-noto-nerd noto-fonts ttf-font-awesome --noconfirm
+echo "Installing pacman things"
+sudo pacman -S fprintd imv ttf-noto-nerd noto-fonts ttf-font-awesome wget --noconfirm
+
+echo "Setting Wallpaper"
+mkdir ~/.config/wallpapers
+wget https://wallpapercave.com/wp/wp11445346.jpg -O ~/.config/wallpapers/wallpaper-1.jpg
+
+echo "Installing yay things"
 yay -S dunst rofi thunderbird unzip neo tlp nwg-look nwg-displays wl-paste eww cliphist hyprpaper iwgtk wlogout blueman pavucontrol hyprpicker grim slurp wpctl brillo brightnessctl microsoft-edge-stable-bin visual-studio-code-bin eza --noconfirm
